23117c450a49c9bdbd0436a98dc8ffca536607a1
Stroev <v.stroev@finch-melrose.com>
2022-02-27 18:43:23 +0300

    fix

--- src/com/company/ApplicationMain.java
+++ src/com/company/ApplicationMain.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class ApplicationMain {
 
